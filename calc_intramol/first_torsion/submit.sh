7bba66f91105c0d53da52d9e5a5c76ce9f7e262d
maximilian fleck <fleck@itt.uni-stuttgart.de>
2023-11-27 13:53:15 +0100

    ini

--- calc_intramol/first_torsion/submit.sh
+++
@@ -1,1 +0,0 @@
-sbatch -p single --export=ALL,OMP_NUM_THREADS=8 -J B3LYP -N 1 -c 8 -t 24:10:00 --mem=2000 ./scan_start.sh
--- calc_intramol/non_dft/submit.sh
+++
@@ -1,1 +0,0 @@
-sbatch -p single --export=ALL,OMP_NUM_THREADS=8 -J mp2 -N 1 -c 8 -t 70:00:00 --mem=2000 ./scan_start.sh
